d5cbe5beb6b1efc2dabb5036404859e54ba253dc
Cláudia Aguilar <aguilarp222@gmail.com>
2022-04-21 23:49:23 -0300

    Terminando estilização

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # php_login
 Projeto proposto no curso Programador Web Senac
+Paleta: ffffff  - 00b295   - 2d3142    -  a4031f   - f4ecd6
--- login.php
+++ login.php
@@ -6,10 +6,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
     <title>AdotPet</title>
 </head>
 <body>
-    
+    <header>
+        <h1>AdotPet</h1>
+       
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Sobre</a>
+            <a href="#">Contato</a>
+            <a href="#">Login</a>
+        </nav>
+    </header>
+    <main>
+        <h2>Seja bem vindo!</h2>
+       
+        <form action="" method="post">
+            <ul>
+                <li>
+                    <label for="">Usuário</label>
+                    <input type="text" name="" id="">
+                </li>
+                <li>
+                    <label for="">Senha</label>
+                    <input type="number" name="" id="">
+                </li>
+                <li>
+                    <button type="submit">Entrar</button>
+                    <button type="reset">Limpar</button>
+                </li>
+            </ul>
+        </form>
+
+    </main>
+    <footer>
+        <p>Atividade proposta no curso Programador Web Senac</p>
+        <p>Cláudia Aguilar</p>
+    </footer>
     
 </body>
 </html>
